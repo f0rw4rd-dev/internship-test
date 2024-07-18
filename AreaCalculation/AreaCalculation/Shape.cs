03d3ba2c4c8ebd9aa34b5efa4f02b7e82a7fa55a
f0rw4rd-dev <52598385+f0rw4rd-dev@users.noreply.github.com>
2024-07-18 18:34:16 +0300

    Add comments

--- AreaCalculation/AreaCalculation/Shape.cs
+++ AreaCalculation/AreaCalculation/Shape.cs
@@ -2,8 +2,19 @@
 {
     public abstract class Shape
     {
+        /// <summary>
+        /// Вычисляет площадь фигуры
+        /// </summary>
+        /// <returns>Площадь фигуры</returns>
         public abstract double CalculateArea();
 
+        /// <summary>
+        /// Валидирует, что заданное значение является положительным
+        /// </summary>
+        /// <param name="value">Значение</param>
+        /// <param name="propertyName">Название параметра</param>
+        /// <returns>Заданное значение, если оно является положительным</returns>
+        /// <exception cref="ArgumentOutOfRangeException">Возникает, если значение меньше или равно нулю</exception>
         protected static double ValidatePositiveValue(double value, string propertyName)
         {
             if (value <= 0)
